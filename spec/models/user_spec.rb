8832dd60c2e396384af41c22ee96355061730dd4
RaviShankarArya <rshankar@qwinixtech.com>
2014-12-02 14:31:53 +0530

    Model spec for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,15 +15,22 @@ RSpec.describe User, :type => :model do
 
     it { should validate_presence_of :first_name }
     it { should allow_value('Ravi').for(:first_name)}
+
     it { should validate_presence_of :last_name }
     it { should allow_value('Shankar').for(:last_name)}
+
     it { should validate_presence_of :password }
     it { should allow_value('Password_123@').for(:password)}
+
     it { should validate_presence_of :password_confirmation }
     it { should allow_value('Password_123@').for(:password_confirmation)}
+
     it { should validate_presence_of :email }
+    it { should validate_uniqueness_of(:email).with_message(/Email already exists/)}
     it { should allow_value('ABCDE@domain.com').for(:email)}
+
     it { should validate_presence_of :address }
+
     it { should validate_presence_of :phone }
     it { should allow_value('990-191-6142').for(:phone)}
 
